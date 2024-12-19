c74d7c8ef5b90e8d3a4a4a4b36cba91f28b28108
sylsylviane <syl.sylviane1@gmail.com>
2024-12-19 13:33:56 -0500

    efSP changer le nom categories pour pays

--- js/filtrepays.js
+++ js/filtrepays.js
@@ -1,41 +1,41 @@
-let categorie = 49;
+let pays = 0;
 
-extraire_categories(categorie);
+extraire_pays(pays);
 
-let bouton_categorie = document.querySelectorAll(".filtre__bouton button"); //
+let bouton_pays = document.querySelectorAll(".filtre__bouton button"); //
 // Ajoute un écouteur d’événements pour chaque bouton de catégorie
-for (const elm of bouton_categorie) {
+for (const elm of bouton_pays) {
   elm.addEventListener("mousedown", function (e) {
     // Lorsqu’un bouton est cliqué, on récupère l’ID de la catégorie associée
-    categorie = e.target.dataset.id;
-    // console.log("Catégorie sélectionnée: ", categorie);
+    pays = e.target.dataset.id;
+    console.log("Pays sélectionnée: ", pays);
     // Appel de la fonction pour extraire les cours de la catégorie sélectionnée
-    extraire_categories(categorie);
+    extraire_pays(pays);
     // Lorsque le DOM est complètement chargé, on lance la récupération des cours (au cas où une catégorie serait pré-sélectionnée)
-    document.addEventListener("DOMContentLoaded", extraire_categories);
-    // Fonction pour extraire les cours via l’API REST de WordPress
+    document.addEventListener("DOMContentLoaded", extraire_pays);
+    
   });
 }
 
-function extraire_categories(categorie) {
+function extraire_pays(pays) {
   // Construction de l’URL pour appeler l’API REST en fonction de la catégorie
   fetch(
-    `http://localhost/31w/wp-json/wp/v2/posts?search=${categorie}&per_page=30`
+    `http://localhost/31w/wp-json/wp/v2/posts?search=${pays}&per_page=30`
   )
     // Conversion de la réponse en JSON
     .then((response) => response.json())
     .then((data) => {
       // Affiche les articles récupérés pour déboguer
       console.log("Articles récupérés: ", data);
-      afficherArticles(data); // Appel à la fonction pour afficher les articles
+      afficherVoyages(data); // Appel à la fonction pour afficher les articles
     })
     .catch((error) =>
       console.error("Erreur lors de l’extraction des destinations: ", error)
     );
 }
 
-function afficherArticles(articles) {
-  const conteneurResultat = document.querySelector(".filtre__resultat");
+function afficherVoyages(articles) {
+  const conteneurResultat = document.querySelector(".filtre__pays");
   conteneurResultat.classList.add("accordion");
   conteneurResultat.innerHTML = "";
   articles.forEach(function (element, index) {
